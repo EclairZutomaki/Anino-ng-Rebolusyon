6e2b348b6eb00c9de35590b2afc566b227ce1683
nyasuwi <eneasalayan25@gmail.com>
2025-12-03 04:40:50 +0800

    Initial commit

---
+++ Anino ng Rebulosyon/Assets/Script/SoundSetting.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundSetting : MonoBehaviour
+{
+    [SerializeField] Slider soundSlider;
+    [SerializeField] AudioMixer masterMixer;
+
+    private void Start()
+    {
+        SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 100));
+    }
+
+    private void SetVolume(float _value)
+    {
+        if (_value < 1)
+        {
+            _value = .001f;
+        }
+
+        RefreshSlider(_value);
+        PlayerPrefs.SetFloat("SavedMasterVolume", _value);
+        masterMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);
+    }
+
+    public void SetVolumeFromSlider()
+    {
+        SetVolume(soundSlider.value);
+    }
+
+    private void RefreshSlider(float _value)
+    {
+        soundSlider.value = _value;
+    }
+}
